da1d852d458987ef2e682b668124e432206dd9a2
mglhbr <120033327+mglhbr@users.noreply.github.com>
2023-02-22 20:58:14 +0100

    Solved exercises 1 to 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(char, num) {
+    if (num < 0) return "ERROR";
+    let sentence = ''
+    for(let i = 0; i<num; i++) {
+        sentence = sentence + char
+    }
+    return sentence
 };
 
 // Do not edit below this line
